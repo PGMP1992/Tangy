311253bc840ec972f691feae7db248a6acc27c65
Pedro Martinez <pedro@dkltd.net>
2025-01-20 14:51:16 +0100

    Changed to use Minimun API calls instead of Controllers

---
+++ TangyWeb_API/Cont/OrderController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Tangy_Business.Repos.IRepos;
+using Tangy_Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TangyWeb_API.Controllers
+{
+    //[Route("api/[controller]/[action]")]
+    //[ApiController]
+
+    //public class OrderController : ControllerBase
+    //{
+    //    private readonly IOrderRepos _orderRepos;
+    //    private readonly IEmailSender _emailSender;
+
+    //    public OrderController(IOrderRepos orderRepos, IEmailSender emailSender)
+    //    {
+    //        _orderRepos = orderRepos;
+    //        _emailSender = emailSender;
+    //    }
+    //    // GET: api/<OrderController>
+    //    [HttpGet]
+    //    public async Task<ActionResult> GetAll()
+    //    {
+    //        return Ok(await _orderRepos.GetAll());
+    //    }
+
+    //    // GET api/<OrderController>/5
+    //    [HttpGet("{orderHeaderId}")]
+    //    public async Task<IActionResult> Get(int? orderHeaderId)
+    //    {
+    //        if (orderHeaderId == null || orderHeaderId == 0)
+    //        {
+    //            return BadRequest(new ErrorModelDTO()
+    //            {
+    //                ErrorMessage = "Invalid Id",
+    //                StatusCode = StatusCodes.Status400BadRequest
+    //            });
+    //        }
+
+    //        var orderHeader = await _orderRepos.Get(orderHeaderId.Value);
+    //        if (orderHeader == null)
+    //        {
+    //            return BadRequest(new ErrorModelDTO()
+    //            {
+    //                ErrorMessage = "Invalid Id",
+    //                StatusCode = StatusCodes.Status404NotFound
+    //            });
+    //        }
+
+    //        return Ok(orderHeader);
+    //    }
+
+    //    //[HttpPost]
+    //    //[ActionName("Create")]
+    //    //public async Task<IActionResult> Create([FromBody] StripePaymentDTO paymentDTO)
+    //    //{
+    //    //    paymentDTO.Order.OrderHeader.OrderDate = DateTime.Now;
+    //    //    var result = await _orderRepos.Create(paymentDTO.Order);
+    //    //    return Ok(result);
+    //    //}
+
+    //    //[HttpPost]
+    //    //[ActionName("paymentsuccessful")]
+    //    //public async Task<IActionResult> PaymentSuccessful([FromBody] OrderHeaderDTO orderHeaderDTO)
+    //    //{
+    //    //    var service = new SessionService();
+    //    //    var sessionDetails = service.Get(orderHeaderDTO.SessionId);
+    //    //    if (sessionDetails.PaymentStatus == "paid")
+    //    //    {
+    //    //        var result = await _orderRepos.MarkPaymentSuccessful(orderHeaderDTO.Id);
+    //    //        await _emailSender.SendEmailAsync(orderHeaderDTO.Email, "Tangy Order Confirmation",
+    //    //            "New Order has been created :" + orderHeaderDTO.Id);
+    //    //        if (result == null)
+    //    //        {
+    //    //            return BadRequest(new ErrorModelDTO()
+    //    //            {
+    //    //                ErrorMessage = "Can not mark payment as successful"
+    //    //            });
+    //    //        }
+    //    //        return Ok(result);
+    //    //    }
+    //    //    return BadRequest();
+    //    //}
+    //}
+}
--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -7,9 +7,10 @@ using TangyWeb_API.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddAuthorization();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //Same as WebServer
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -19,7 +20,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICategoryRepos, CategoryRepos>();
 builder.Services.AddScoped<IProductRepos, ProductRepos>();
 builder.Services.AddScoped<IProductPriceRepos, ProductPriceRepos>();
-
+builder.Services.AddScoped<IOrderRepos, OrderRepos>();
 // CORS 
 builder.Services.AddCors(o => o.AddPolicy("Tangy", builder =>
 {
@@ -31,7 +32,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
+    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -42,14 +43,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
 
 // Minimum API Below... moved inside /Endpoints/ProductEndpoints() 
 //app.MapGet("/api/products", async (AppDbContext context) =>
 //    await context.Products.ToListAsync());
 
 app.MapProductEndpoints(); // Extension for Product EndPoints => Endpoints Folder. 
-//app.MapOrderEndpoints(); // Extension for OrderEndPoints => Endpoints Folder. 
+app.MapOrderEndpoints(); // Extension for OrderEndPoints => Endpoints Folder. 
 
 app.Run();
 
